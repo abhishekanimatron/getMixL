d6ccdfece48d597f1e991c95cdc950e3f8471074
abhishekanimatron <abhishekverma297ab@gmail.com>
2021-05-09 22:55:54 +0530

    reinitial

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAX9qi4y6pC7C-0esgB_KgE7ItP6rl5uoU",
+  authDomain: "sicksock-98175.firebaseapp.com",
+  projectId: "sicksock-98175",
+  storageBucket: "sicksock-98175.appspot.com",
+  messagingSenderId: "491470925194",
+  appId: "1:491470925194:web:522df9acb68b0261162c73",
+};
